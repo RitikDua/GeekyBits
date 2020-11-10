864270a50b6cf911bdbffaee7a63786a81389d01
RitikDua <rittikdua@gmail.com>
2020-11-10 16:15:29 +0530

    docs: Default Theme Updated 	-- Primary colorTheme of Material Ui is Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
-import Main from './components/Main'
+import Main from './components/Main';
+import Dashboard from './components/Dashboard/Dashboard';
+import {createMuiTheme} from '@material-ui/core/styles';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+//To Change Default Material UI Theme
+const theme=createMuiTheme({
+	palette:{
+		primary:{
+			500:"rgb(7, 54, 64);"
+		}
+	}
+});
+
+
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+    	<CssBaseline />
     <div className="App">
-      <Main/>
+      <Dashboard/>{/* Testing */}
     </div>
+    </ThemeProvider>
   );
 }
 
