57972fdcb9193a20913d889cd6f3697181cea598
MilanshuRathi <milanshu2000@gmail.com>
2020-11-12 19:04:24 +0530

    chore: Repository refactored

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors=require('cors');
 const morgan = require('morgan');
-
+const tutFile=require(`${__dirname}/dev-data/tutorials.json`);
 //Adding middlewares
 if(process.env.NODE_ENV === 'DEVELOPMENT')
     app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 //mounting api endpoints
 app.route('/')
@@ -13,5 +15,16 @@ app.route('/')
         status: 'success',
         message: 'Server is running'
     });
+});
+app.route('/tutorials')
+.get((request, response) => {
+    response.status(200).json(tutFile);
+});
+app.route('/tutorials/:tutId')
+.get((request, response) => {
+    const object=tutFile.find(obj=>{
+        return obj._id===parseInt(request.params.tutId);
+    });
+    response.status(200).json(object);
 });
 module.exports =app;
