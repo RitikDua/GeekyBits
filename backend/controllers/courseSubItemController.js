dede12831db6edab2d0a6cf7f0160b30cc263fd6
MilanshuRathi <milanshu2000@gmail.com>
2020-11-15 00:17:11 +0530

    refactor: route functions refactored to controllers

---
+++ backend/controllers/courseSubItemController.js
@@ -0,0 +1,40 @@
+const CourseSubItems=require(`${__dirname}/../models/courseSubItemModel`);
+exports.getAllCourseSubItems=async (request,response)=>{
+    try{        
+        const courseSubItems=await CourseSubItems.find().populate('subItem');
+        response.status(200).json({
+            status:'success',
+            data:{
+                courseSubItems
+            }
+        });
+    }
+    catch (err){
+        response.status(500).json({
+            status:'error',
+            message:err.message,
+            name:err.name,
+            err
+        });
+    }
+};
+exports.getCourseSubItemById=async (request,response)=>{
+    try{
+        const courseSubItemId=request.params.courseSubItemId;        
+        const courseSubItem=await CourseSubItems.findById(courseSubItemId).populate('subItem');
+        response.status(200).json({
+            status:'success',
+            data:{
+                courseSubItem
+            }
+        });
+    }
+    catch (err){
+        response.status(500).json({
+            status:'error',
+            message:err.message,
+            name:err.name,
+            err
+        });
+    }
+};
