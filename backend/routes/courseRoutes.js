428f03e57f35e8d2ab4bbb8c1960b6d72dc32793
MilanshuRathi <milanshu2000@gmail.com>
2020-11-19 11:10:46 +0530

    chore: protected routes

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router=express.Router();
+const authController=require(`${__dirname}/../controllers/authController`);
 const courseController=require(`${__dirname}/../controllers/courseController`);
+router.use(authController.protect);
 router.route('/')
 .get(courseController.getAllCourses);
 router.route('/:courseId')
--- backend/routes/courseSubItemRoutes.js
+++ backend/routes/courseSubItemRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router=express.Router();
+const authController=require(`${__dirname}/../controllers/authController`);
 const courseSubItemController=require(`${__dirname}/../controllers/courseSubItemController`);
+router.use(authController.protect);
 router.route('/')
 .get(courseSubItemController.getAllCourseSubItems);
 router.route('/:courseSubItemId')
