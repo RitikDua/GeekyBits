6c24ce2dfa7d462c564ee09e8ffcaef4d7f0ddeb
03sid-sharma <03sid.sharma@gmail.com>
2020-11-21 01:32:31 +0530

    chore: repo refactored

--- backend/routes/attemptsRoutes.js
+++ backend/routes/attemptsRoutes.js
@@ -1,34 +1,31 @@
-const express = require("express");
-const axios = require("axios");
 const { Attempt } = require(`${__dirname}/../models/attemptsModel`);
+const mongoose = require("mongoose");
+const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const code = decodeURIComponent(req.body.script);
-  var program = {
-    script: code,
-    language: req.body.language,
-    versionIndex: "3",
-    clientId: process.env.JDOODLE_CLIENT_KEY,
-    clientSecret: process.env.JDOODLE_SERVER_KEY,
-  };
   try {
-    const { data } = await axios({
-      method: "POST",
-      url: "https://api.jdoodle.com/v1/execute",
-      data: program,
-    });
-
-    res.status(200).json({
+    const attemptObject = {
+      attemptType: req.body.attemptType,
+      attemptString: req.body.attemptString,
+      attemptResultDetails: {
+        testCasesPassed: req.body.testCasesPassed,
+        testCasesUserOutputs: req.body.testCasesUserOutputs,
+      },
+      problem: req.body.problemId,
+      user: req.body.userId,
+    };
+    const attempt = await Attempt.create(attemptObject);
+    console.log(attempt);
+    res.status(201).json({
       status: "success",
-      output: data,
+      data: attempt,
     });
   } catch (err) {
     res.status(500).json({
       status: "error",
-      err,
+      err: err.message,
     });
   }
 });
-
 module.exports = router;
