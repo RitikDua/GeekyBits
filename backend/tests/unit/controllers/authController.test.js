dcca34596a5c8a6be8c0b73a98d1eda89365bd8a
03sid-sharma <03sid.sharma@gmail.com>
2020-11-24 22:53:01 +0530

    chore: tests initialized

---
+++ backend/tests/unit/controllers/authController.test.js
@@ -0,0 +1,7 @@
+const Users= require(`${__dirname}/../../../models/userModel`);
+describe('Generate Authentication Token', () => {
+    it('should return a valid JWT', () => {
+        const user = new User({_id: 1});
+        
+    });
+});
