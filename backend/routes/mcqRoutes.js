3bcb24e1b84c3b9cec50f400eeead3bb36cdd5f2
MilanshuRathi <milanshu2000@gmail.com>
2020-11-26 23:30:50 +0530

    chore: added create routes

--- backend/routes/mcqRoutes.js
+++ backend/routes/mcqRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router=express.Router();
+const authController=require(`${__dirname}/../controllers/authController`);
 const mcqController=require(`${__dirname}/../controllers/mcqController`);
+router.use(authController.protect);
 router.route('/')
-.get(mcqController.getMCQs);
+.get(mcqController.getMCQs)
+.post(mcqController.createMCQ);
 router.route('/:mcqId')
 .get(mcqController.getMCQById);
 module.exports=router;
