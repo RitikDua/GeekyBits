aa996db473228c3921055f9231b0094ff3de6179
RitikDua <rittikdua@gmail.com>
2020-11-26 13:10:33 +0530

    fix: minor bugs fixed

--- backend/controllers/attemptController.js
+++ backend/controllers/attemptController.js
@@ -51,10 +51,10 @@ exports.getAttemptsByProblemId=async (request,response)=>{
 };
 exports.submitAttempt=async (request, response) => {
     try{
-        const { problemId, userId, attemptType, attemptString, attemptLanguage,attemptTitle } = request.body;//.problemId;
-        const attemptObj = {attemptType,attemptString,problem: problemId,user: userId};
+        const { problemId, userId, attemptType, attemptString, attemptLanguage,attemptTitle,subItemId } = request.body;//.problemId;
+        const attemptObj = {attemptType,attemptString,problem: problemId,user: userId,subItem:subItemId,attemptTitle};
         if (attemptType === "CodingProblem") {
-            const codingProblem = await CodingProblems.findById(problemId);
+            const codingProblem = await CodingProblems.findById(subItemId);
             const { testCases, correctOutput } = codingProblem;
             let arr = [];
             testCases.forEach(testCase => arr.push(executeCode.executeCode(attemptString,testCase)));
@@ -69,7 +69,8 @@ exports.submitAttempt=async (request, response) => {
             attemptObj.testCasesPassed=checkTestCases;
             attemptObj.testCasesUserOutputs=arr;
             attemptObj.attemptLanguage=attemptLanguage;
-            attemptObj.attemptTitle=attemptTitle;
+            // attemptObj.attemptTitle=attemptTitle;
+            // attemptObj.subItemId=problemId;
             deleteFile(`${__dirname}/../input.txt`);
             deleteFile(`${__dirname}/../test.c`);
             deleteFile(`${__dirname}/../a.out`);             
