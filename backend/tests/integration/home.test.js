3e867de51ce9c03dd356ffea092f4a53ba3311ff
03sid-sharma <03sid.sharma@gmail.com>
2020-11-27 23:56:58 +0530

    test: major tests added

---
+++ backend/tests/integration/home.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+describe('/users', () => {
+    beforeEach(() => {server = require('../../server');});
+    afterEach(() => {
+        server.close();
+        // await Users.deleteMany({});
+    });
+
+    describe('GET /', () => {
+        it('should return Home Page',async() => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+    });
+});
