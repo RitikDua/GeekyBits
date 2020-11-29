4b7bd96441a212886b42852ec33e4585ad274df2
03sid-sharma <03sid.sharma@gmail.com>
2020-11-30 03:51:54 +0530

    test: code execution test added

--- backend/tests/unit/middlewares/compileCode.test.js
+++ backend/tests/unit/middlewares/compileCode.test.js
@@ -1,14 +1,37 @@
-const {executeCode,saveFile} = require('../../../middlewares/CompileCode');
-const fs = require('fs');
+const ccd = require("../../../middlewares/CompileCode");
+const Users = require("../../../models/userModel");
 
-describe('Compile Code Middleware',() => {
-    it('should save the file',() =>{
-        const req = {
-            header: jest.fn().mockReturnValue(token)
-        };
-        const res={};
-        const next = jest.fn();
-        executeCode(req,res);
-        expect(req.user).toMatchObject(user);
-    });
+describe("Compile Code Middleware", () => {
+  it("should execute the code", async () => {
+    try {
+      ccd.executeCode = jest.fn();
+
+      ccd.executeCode.mockResolvedValue({
+        err: false,
+        output: "Hello World",
+      });
+
+      const result = await ccd.executeCode();
+      expect(result).toHaveBeenCalled();
+      expect(ccd.executeCode.mock.calls[0][0]).toBe("Hello World");
+      expect(ccd.executeCode.mock.calls[0][1]).toBe(1);
+    } catch (error) {
+      console.log("Something goes terribly wrong!");
+    }
+  });
+
+  it("should throw error for wrong code", async () => {
+    try {
+      ccd.executeCode = jest.fn();
+
+      ccd.executeCode.mockRejectedValue(new Error("EXECUTION FAILED"));
+
+      const result = await ccd.executeCode();
+
+      expect(result).toHaveBeenCalled();
+      expect(result).toThrow(Error);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 });
