44b802235b27a1ff75d4ff9df050a0f33aad6531
MilanshuRathi <milanshu2000@gmail.com>
2020-11-29 11:41:50 +0530

    feat: added contest routes

---
+++ backend/routes/contestRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { route } = require('../app');
+const router = express.Router();
+const authController=require(`${__dirname}/../controllers/authController`);
+const contestController=require(`${__dirname}/../controllers/contestController`);
+router.use(authController.protect);
+router.route('/')
+.get(contestController.getContests)
+.post(contestController.createContest)
+.delete(contestController.deleteContests);
+router.route('/:contestId')
+.get(contestController.getContestById)
+.post(contestController.registerParticipant)
+.patch(contestController.updateContest);
+router.route('/:contestId/:contestUrl')
+.get(contestController.startContest)
+module.exports = router;
