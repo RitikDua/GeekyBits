fcb746db1e19048a2c5a1355cd4d99872c9495eb
MilanshuRathi <milanshu2000@gmail.com>
2020-12-02 16:02:15 +0530

    chore: added create item functions

--- backend/controllers/courseSubItemController.js
+++ backend/controllers/courseSubItemController.js
@@ -1,4 +1,5 @@
 const CourseSubItems=require(`${__dirname}/../models/courseSubItemModel`);
+const {matchBodyWithSchema}=require(`${__dirname}/../utils/matchBodyWithSchema`);
 exports.getCourseSubItems=async (request,response)=>{
     try{        
         const courseSubItems=await CourseSubItems.find();
@@ -38,3 +39,23 @@ exports.getCourseSubItemById=async (request,response)=>{
         });
     }
 };
+exports.createCourseSubItem=async (request,response)=>{
+    try{
+        const courseSubItemDetails = request.body;
+        if(!matchBodyWithSchema(Object.keys(courseSubItemDetails),['subItemTitle','subItemType','subItem']))
+            return response.status(400).json({status:'error',message:'Please provide coursesubitem details'});
+        const courseSubItem=await CourseSubItems.create(courseSubItemDetails);
+        response.status(201).json({
+            status:'success',
+            data:{courseSubItem}
+        });
+    }
+    catch (err){
+        response.status(500).json({
+            status:'error',
+            message:err.message,
+            name:err.name,
+            err
+        });
+    }
+}
