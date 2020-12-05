f7eed6f89047b057160ae46ca56ebf2c16598333
unknown <dgargdhruv@gmail.com>
2020-12-05 21:50:09 +0530

    refactor: register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -3,14 +3,20 @@ import { Button, Grid, TextField} from '@material-ui/core';
 import Axios from 'axios';
 function Register() {
     const [url, seturl] = useState("");
+    const [contest, setcontest] = useState("");
     const handleSub=(e)=>{
         e.preventDefault();
         Axios.post(url)
         .then((res)=>{
-            console.log(res);
+            console.log(res.data.contestStartUrl);
+            setcontest(res.data.contestStartUrl);
         }).catch((err)=>{
             console.log(err);
         })
+    }
+    const handleRedirect=()=>{
+        localStorage.setItem("contest-url",contest);
+        window.location.href="/contestmain";
     }
     return (
         <div style={{paddingTop:"50px"}}>
@@ -20,7 +26,8 @@ function Register() {
            <form noValidate autoComplete="off" onSubmit={(e)=>handleSub(e)}>
             <TextField value={url} onChange={(e)=>seturl(e.target.value)} style={{width:"400px",height:"40px"}} id="outlined-basic"  variant="outlined" /><br/><br/><br/>
             <Button style={{width:"400px"}} type="submit" variant="outlined" color="primary">Register</Button>
-            </form></div>
+            </form></div><br/><br/>
+            {contest && <div style={{display:"flex",paddingLeft:"20%"}}><div style={{fontSize:"25px"}}><span>Contest-URL: &nbsp;&nbsp;</span></div><div onClick={()=>handleRedirect()} style={{backgroundColor:"#F5F5F5",width:"550px",padding:"1% 2%"}}><span>{contest}</span></div></div>}<br/>
         </div>
         </div>
     )
