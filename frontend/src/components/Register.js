5a546a1c617666e834ee5a635c546f9062b78a01
unknown <dgargdhruv@gmail.com>
2020-12-05 22:30:02 +0530

    refactor: register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -22,7 +22,7 @@ function Register() {
         <div style={{paddingTop:"50px"}}>
              <div style={{textAlign:"center",backgroundColor:"white",height:"100vh"}}>
             <div style={{paddingTop:"10%"}}>
-           <h1>Have you got Register-URL?</h1><br/><br/>
+           <h1>Are you ready for contest?</h1><br/>
            <form noValidate autoComplete="off" onSubmit={(e)=>handleSub(e)}>
             <TextField value={url} onChange={(e)=>seturl(e.target.value)} style={{width:"400px",height:"40px"}} id="outlined-basic"  variant="outlined" /><br/><br/><br/>
             <Button style={{width:"400px"}} type="submit" variant="outlined" color="primary">Register</Button>
