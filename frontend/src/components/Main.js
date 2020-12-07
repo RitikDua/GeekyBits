905a8222dc8fdfc6a8cac222bccc9507cac4bc8f
unknown <dgargdhruv@gmail.com>
2020-12-07 22:42:43 +0530

    fix: minor issue

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import Banner from './Banner';
-import Nav from './Nav';
-import Flow from './Flow';
-import Gridblock from './Gridblock';
-import Logoblock from './Logoblock';
-import Footer from "./Footer";
-import Dashboard from './Dashboard/Dashboard';
-import { Route, Switch,Link } from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 import MainRoutes from '../routes/MainRoutes';
 function Main() {
     return ( 
