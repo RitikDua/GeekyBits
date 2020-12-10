02639561e7c36257778f88f32c1760a88dfbdd5a
03sid-sharma <03sid.sharma@gmail.com>
2020-12-10 22:58:31 +0530

    chore: added validations in controllers

--- backend/controllers/attemptController.js
+++ backend/controllers/attemptController.js
@@ -1,121 +1,160 @@
 const CodingProblems = require(`${__dirname}/../models/codingProblemModel`);
-const Attempts=require(`${__dirname}/../models/attemptModel`);
-const executeCode=require(`${__dirname}/../middlewares/CompileCode`);
-const {updateProgress}=require(`${__dirname}/../utils/updateProgress`);
-const {c, cpp, python, java} = require('compile-run');
-const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
+const { updateProgress } = require(`${__dirname}/../utils/updateProgress`);
+const catchAsyncError = require(`${__dirname}/../utils/catchAsyncError`);
+const executeCode = require(`${__dirname}/../middlewares/CompileCode`);
+const Attempts = require(`${__dirname}/../models/attemptModel`);
 const AppError = require(`${__dirname}/../utils/AppError`);
+const { c, cpp, python, java } = require("compile-run");
+const mongoose = require("mongoose");
+
 const deleteFile = (filename) => {
-    fs.unlink(filename, function (err) {
-        if (err) {
-            console.log("SORRY NOT DELETED")
-        };
-        // if no error, file has been deleted successfully
-        console.log('File deleted!');
-    }); 
-}
-exports.getAttempts=catchAsyncError(async (request,response,next)=>{
-    let filterObj={};
-    if(request.user.role==='admin')            
-        filterObj=request.body;
-    else
-        filterObj.user=request.user._id;
-    const attempts=await Attempts.find(filterObj).sort({createdAt:-1});        
-    response.status(200).json({
-        status:'success',
-        data:{attempts}
-    });
+  fs.unlink(filename, function (err) {
+    if (err) {
+      console.log("SORRY NOT DELETED");
+    }
+    // if no error, file has been deleted successfully
+    console.log("File deleted!");
+  });
+};
+exports.getAttempts = catchAsyncError(async (request, response, next) => {
+  let filterObj = {};
+  if (request.user.role === "admin") filterObj = request.body;
+  else filterObj.user = request.user._id;
+  const attempts = await Attempts.find(filterObj).sort({ createdAt: -1 });
+  response.status(200).json({
+    status: "success",
+    data: { attempts },
+  });
 });
-exports.getAttemptsByProblemId=catchAsyncError(async (request,response,next)=>{    
-    const filterObj={problem: request.params.problemId};
-    if(request.user.role==='user')
-        filterObj.user=request.user._id;
-    const attempts=await Attempts.find(filterObj).sort({createdAt:-1});;
+exports.getAttemptsByProblemId = catchAsyncError(
+  async (request, response, next) => {
+    if (!mongoose.Types.ObjectId.isValid(request.params.problemId))
+      return next(new AppError("Invalid ID", 400));
+
+    const filterObj = { problem: request.params.problemId };
+    if (request.user.role === "user") filterObj.user = request.user._id;
+    const attempts = await Attempts.find(filterObj).sort({ createdAt: -1 });
     response.status(200).json({
-        status:'success',
-        data:{attempts}
+      status: "success",
+      data: { attempts },
     });
-});
-exports.submitAttempt=catchAsyncError(async (request, response,next) => {
-    const { problemId,attemptType, attemptString, attemptLanguage,attemptTitle,subItemId,attemptResult,courseId} = request.body;//.problemId;
-    const attemptObj = {attemptType,attemptString,problem: problemId,user:request.user._id,subItem:subItemId,attemptTitle,attemptResult};        
-    if (attemptType === "CodingProblem") {
-        const codingProblem = await CodingProblems.findById(subItemId);
-        const { testCases, correctOutput } = codingProblem;            
-        let arr = [];
-        let testEnv;
-        switch (attemptLanguage.toLowerCase()){
-            case 'c':testEnv=c;break;
-            case 'c++':testEnv=cpp;break;
-            case 'java':testEnv=java;break;
-            case 'python':testEnv=python;break;
-        }
-        testCases.forEach(testCase =>{                
-            arr.push(testEnv.runSource(attemptString,{stdin:decodeURIComponent(testCase)}))
-        });
-        // let error="";
-        // testCases.forEach(testCase => arr.push(executeCode.executeCode(attemptString,testCase)));
+  }
+);
+exports.submitAttempt = catchAsyncError(async (request, response, next) => {
+  const {
+    problemId,
+    attemptType,
+    attemptString,
+    attemptLanguage,
+    attemptTitle,
+    subItemId,
+    attemptResult,
+    courseId,
+  } = request.body; //.problemId;
+  const attemptObj = {
+    attemptType,
+    attemptString,
+    problem: problemId,
+    user: request.user._id,
+    subItem: subItemId,
+    attemptTitle,
+    attemptResult,
+  };
+  if (attemptType === "CodingProblem") {
+    const codingProblem = await CodingProblems.findById(subItemId);
+    const { testCases, correctOutput } = codingProblem;
+    let arr = [];
+    let testEnv;
+    switch (attemptLanguage.toLowerCase()) {
+      case "c":
+        testEnv = c;
+        break;
+      case "c++":
+        testEnv = cpp;
+        break;
+      case "java":
+        testEnv = java;
+        break;
+      case "python":
+        testEnv = python;
+        break;
+    }
+    testCases.forEach((testCase) => {
+      arr.push(
+        testEnv.runSource(attemptString, {
+          stdin: decodeURIComponent(testCase),
+        })
+      );
+    });
+    // let error="";
+    // testCases.forEach(testCase => arr.push(executeCode.executeCode(attemptString,testCase)));
     //    for(let i of testCases){
     //        const x=executeCode.executeCode(attemptString,i);
-            
+
     //        x.then((y)=>{
     //         if(y.err){
-                    // console.log("asd");                   
-                // console.log(JSON.stringify(y))
+    // console.log("asd");
+    // console.log(JSON.stringify(y))
     //            error=y.output.cmd.startsWith("gcc")?"compiler error":"runtime error";
     //        console.log(error);
     //        }
-            
+
     //        })
     //        arr.push(x);
-    //    } 
+    //    }
 
-
-        const result = await Promise.all(arr);
-        arr = [];
-        let checkTestCases = [];
-        let checkStatus=0,errorType='';
-        for (let i = 0; i < result.length; i++) {
-            let outcome;     
-            // console.log(result[i]);           
-            if(result[i].stderr||result[i].signal||result[i].exitCode) {                    
-                errorType = result[i].errorType==='run-time'||result[i].signal?'runtime error':'compiler error';            
-                outcome=false;
-            }
-            else                    
-            outcome=(result[i].stdout===decodeURIComponent(correctOutput[i]));
-            // const outcome=(result[i].output===decodeURIComponent(correctOutput[i]));
-            checkStatus+=outcome;
-            checkTestCases.push(outcome);
-            arr.push(result[i].stdout?result[i].stdout:result[i].stderr);
-        }                       
-        //     arr.push(result[i].output);
-        // }
-            attemptObj.attemptResult=errorType?errorType:((checkStatus===result.length)?"correct":"wrong");
-        // attemptObj.attemptResult=error?error:(checkStatus===result.length)?"correct":"wrong";
-        attemptObj.testCasesPassed=checkTestCases;
-        attemptObj.testCasesUserOutputs=arr;
-        attemptObj.attemptLanguage=attemptLanguage;   
-        // deleteFile(`${__dirname}/../input.txt`);
-        // deleteFile(`${__dirname}/../test.c`);
-        // deleteFile(`${__dirname}/../a.out`);                 
-    }  
-    if(courseId&&attemptObj.attemptResult==='correct'){
-        updateProgress(courseId,problemId,request.user);
-    }        
-    const attempt = await Attempts.create(attemptObj);                
-    response.status(201).json({
-        status: "success",
-        data: { attempt }
-    });                             
+    const result = await Promise.all(arr);
+    arr = [];
+    let checkTestCases = [];
+    let checkStatus = 0,
+      errorType = "";
+    for (let i = 0; i < result.length; i++) {
+      let outcome;
+      // console.log(result[i]);
+      if (result[i].stderr || result[i].signal || result[i].exitCode) {
+        errorType =
+          result[i].errorType === "run-time" || result[i].signal
+            ? "runtime error"
+            : "compiler error";
+        outcome = false;
+      } else
+        outcome = result[i].stdout === decodeURIComponent(correctOutput[i]);
+      // const outcome=(result[i].output===decodeURIComponent(correctOutput[i]));
+      checkStatus += outcome;
+      checkTestCases.push(outcome);
+      arr.push(result[i].stdout ? result[i].stdout : result[i].stderr);
+    }
+    //     arr.push(result[i].output);
+    // }
+    attemptObj.attemptResult = errorType
+      ? errorType
+      : checkStatus === result.length
+      ? "correct"
+      : "wrong";
+    // attemptObj.attemptResult=error?error:(checkStatus===result.length)?"correct":"wrong";
+    attemptObj.testCasesPassed = checkTestCases;
+    attemptObj.testCasesUserOutputs = arr;
+    attemptObj.attemptLanguage = attemptLanguage;
+    // deleteFile(`${__dirname}/../input.txt`);
+    // deleteFile(`${__dirname}/../test.c`);
+    // deleteFile(`${__dirname}/../a.out`);
+  }
+  if (courseId && attemptObj.attemptResult === "correct") {
+    updateProgress(courseId, problemId, request.user);
+  }
+  const attempt = await Attempts.create(attemptObj);
+  response.status(201).json({
+    status: "success",
+    data: { attempt },
+  });
 });
-exports.deleteAttempts=async (request,response,next)=>{
-    if(request.user.role!=='admin')
-        return next(new AppError('You are not authorized for this action',403));
-    const attemptDetails=request.body;
-    await Attempts.deleteMany(attemptDetails);
-    response.status(204).json({
-        status:'success',
-        message:'Attempts deleted'
-    });
+exports.deleteAttempts = async (request, response, next) => {
+  if (request.user.role !== "admin")
+    return next(new AppError("You are not authorized for this action", 403));
+  const attemptDetails = request.body;
+  await Attempts.deleteMany(attemptDetails);
+  response.status(204).json({
+    status: "success",
+    message: "Attempts deleted",
+  });
 };
--- backend/controllers/tutorialController.js
+++ backend/controllers/tutorialController.js
@@ -1,34 +1,43 @@
-const Tutorials=require(`${__dirname}/../models/tutorialModel`);
-const catchAsyncError=require(`${__dirname}/../utils/catchAsyncError`);
+const Tutorials = require(`${__dirname}/../models/tutorialModel`);
+const catchAsyncError = require(`${__dirname}/../utils/catchAsyncError`);
 const AppError = require(`${__dirname}/../utils/AppError`);
-const {matchBodyWithSchema}=require(`${__dirname}/../utils/matchBodyWithSchema`);
-exports.getTutorials=catchAsyncError(async (request,response,next)=>{    
-        const tutorials=await Tutorials.find();
-        response.status(200).json({
-            status:'success',
-            data:{tutorials}
-        });         
+const { matchBodyWithSchema } = require(`${__dirname}/../utils/matchBodyWithSchema`);
+const mongoose = require("mongoose");
+
+exports.getTutorials = catchAsyncError(async (request, response, next) => {
+  const tutorials = await Tutorials.find();
+  response.status(200).json({
+    status: "success",
+    data: { tutorials },
+  });
 });
-exports.getTutorialById=catchAsyncError(async (request,response,next)=>{    
-        const tutorialId=request.params.tutorialId;
-        if(!tutorialId)
-            return next(new AppError('Please provide a tutorial id',400));
-        const tutorial=await Tutorials.findById(tutorialId);
-        if (!tutorial)
-            return response.status(404).send("Problem with given ID not found");        
-        
-            response.status(200).json({
-            status:'success',
-            data:{tutorial}
-        });     
+exports.getTutorialById = catchAsyncError(async (request, response, next) => {
+  const tutorialId = request.params.tutorialId;
+  if (!mongoose.Types.ObjectId.isValid(tutorialId))
+    return next(new AppError("Invalid ID", 400));
+
+  const tutorial = await Tutorials.findById(tutorialId);
+  if (!tutorial)
+    return next(new AppError("Tutorial with given ID not found", 404));
+
+  response.status(200).json({
+    status: "success",
+    data: { tutorial },
+  });
 });
-exports.createTutorial=catchAsyncError(async (request,response,next)=>{    
-    const tutorialDetails=request.body;
-    if(!matchBodyWithSchema(Object.keys(tutorialDetails),['tutorialTitle','content','codes']))
-        return next(new AppError('Please provide tutorial details',400));
-    const tutorial=await Tutorials.create(tutorialDetails);
-    response.status(201).json({
-        status:'success',
-        data:{tutorial}
-    });     
+exports.createTutorial = catchAsyncError(async (request, response, next) => {
+  const tutorialDetails = request.body;
+  if (
+    !matchBodyWithSchema(Object.keys(tutorialDetails), [
+      "tutorialTitle",
+      "content",
+      "codes",
+    ])
+  )
+    return next(new AppError("Please provide tutorial details", 400));
+  const tutorial = await Tutorials.create(tutorialDetails);
+  response.status(201).json({
+    status: "success",
+    data: { tutorial },
+  });
 });
