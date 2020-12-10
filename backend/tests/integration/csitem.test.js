7a66d53ac283174acec74d0583b16eb948076600
03sid-sharma <03sid.sharma@gmail.com>
2020-12-10 23:00:20 +0530

    test: added 2 tests for query key in courseSubItem

--- backend/tests/integration/csitem.test.js
+++ backend/tests/integration/csitem.test.js
@@ -24,14 +24,14 @@ describe("/courseSubItems", () => {
     it("should return all CourseSubItems", async () => {
       const dbCourseSubItems = [
         {
-            "subItemType":"Tutorial",
-            "subItem":"5fad8f085503d91a5f6bd398",
-            "subItemTitle":"Introduction%20to%20Programming"
+          subItemType: "Tutorial",
+          subItem: "5fad8f085503d91a5f6bd398",
+          subItemTitle: "Introduction%20to%20Programming",
         },
         {
-            "subItemType":"MCQ",
-            "subItem":"5fad8f085503d91a5f6bd39a",
-            "subItemTitle":"Introduction%20to%20Programming%20-%201"
+          subItemType: "MCQ",
+          subItem: "5fad8f085503d91a5f6bd39a",
+          subItemTitle: "Introduction%20to%20Programming%20-%201",
         },
       ];
 
@@ -42,36 +42,51 @@ describe("/courseSubItems", () => {
         .set("authorization", "Bearer " + token);
 
       expect(res.status).toBe(200);
-      expect(res.body.data.courseSubItems).toHaveProperty("subItemType", dbCourseSubItems.subItemType);
-      expect(res.body.data.courseSubItems.some(csi => csi.subItemTitle === "Introduction%20to%20Programming")).toBeTruthy();
-      expect(res.body.data.courseSubItems.some(csi => csi.subItemTitle === "Introduction%20to%20Programming%20-%201")).toBeTruthy();  
+      expect(res.body.data.courseSubItems).toHaveProperty(
+        "subItemType",
+        dbCourseSubItems.subItemType
+      );
+      expect(
+        res.body.data.courseSubItems.some(
+          (csi) => csi.subItemTitle === "Introduction%20to%20Programming"
+        )
+      ).toBeTruthy();
+      expect(
+        res.body.data.courseSubItems.some(
+          (csi) =>
+            csi.subItemTitle === "Introduction%20to%20Programming%20-%201"
+        )
+      ).toBeTruthy();
     });
   });
 
   describe("GET /:id", () => {
     it("should return subItem if valid id is passed", async () => {
       const prblm = new CourseSubItems({
-        "subItemType":"Tutorial",
-        "subItem":"5fad8f085503d91a5f6bd398",
-        "subItemTitle":"Introduction%20to%20Programming"
-    });
+        subItemType: "Tutorial",
+        subItem: "5fad8f085503d91a5f6bd398",
+        subItemTitle: "Introduction%20to%20Programming",
+      });
 
       await prblm.save();
-      
+
       const res = await request(server)
         .get("/courseSubItems/" + prblm._id)
         .set("authorization", "Bearer " + token);
 
       expect(res.status).toBe(200);
-      expect(res.body.data.courseSubItem).toHaveProperty("subItemTitle", prblm.subItemTitle);
+      expect(res.body.data.courseSubItem).toHaveProperty(
+        "subItemTitle",
+        prblm.subItemTitle
+      );
     });
 
-    it("should return 404 if invalid id is passed", async () => {
+    it("should return 400 if invalid id is passed", async () => {
       const res = await request(server)
         .get("/courseSubItems/1")
         .set("authorization", "Bearer " + token);
 
-      expect(res.status).toBe(404);
+      expect(res.status).toBe(400);
     });
 
     it("should return 404 if no subItem with given id exist", async () => {
@@ -82,11 +97,44 @@ describe("/courseSubItems", () => {
 
       expect(res.status).toBe(404);
     });
+
+    it("should return 400 if invalid courseId key is passed", async () => {
+      const prblm = new CourseSubItems({
+        subItemType: "Tutorial",
+        subItem: "5fad8f085503d91a5f6bd398",
+        subItemTitle: "Introduction%20to%20Programming",
+      });
+
+      await prblm.save();
+
+      const key=123;
+      const res = await request(server)
+        .get("/courseSubItems/" + prblm._id + "?key=" + key)
+        .set("authorization", "Bearer " + token);
+
+      expect(res.status).toBe(400);
+    });
+    it("should return 200 if courseId key is valid", async () => {
+      const prblm = new CourseSubItems({
+        subItemType: "Tutorial",
+        subItem: "5fad8f085503d91a5f6bd398",
+        subItemTitle: "Introduction%20to%20Programming",
+      });
+
+      await prblm.save();
+
+      const key=mongoose.Types.ObjectId();
+      const res = await request(server)
+        .get("/courseSubItems/" + prblm._id + "?key=" + key)
+        .set("authorization", "Bearer " + token);
+
+      expect(res.status).toBe(200);
+    });
   });
 
   describe("POST /", () => {
     // Define
-    let sub={};
+    let sub = {};
 
     const exec = async () => {
       return await request(server)
@@ -97,10 +145,10 @@ describe("/courseSubItems", () => {
 
     beforeEach(() => {
       sub = {
-        "subItemType":"Tutorial",
-        "subItem":"5fad8f085503d91a5f6bd398",
-        "subItemTitle":"Introduction%20to%20Programming"
-    };
+        subItemType: "Tutorial",
+        subItem: "5fad8f085503d91a5f6bd398",
+        subItemTitle: "Introduction%20to%20Programming",
+      };
     });
 
     it("should return 401 if client is not logged in", async () => {
@@ -112,13 +160,13 @@ describe("/courseSubItems", () => {
     });
 
     it("should return 201 and save the courseSubItems if it is valid", async () => {
-      const res=await exec();
+      const res = await exec();
 
       const foo = await CourseSubItems.find({
         sub: {
-            "subItemType":"Tutorial",
-            "subItem":"5fad8f085503d91a5f6bd398",
-            "subItemTitle":"Introduction%20to%20Programming"
+          subItemType: "Tutorial",
+          subItem: "5fad8f085503d91a5f6bd398",
+          subItemTitle: "Introduction%20to%20Programming",
         },
       });
 
@@ -128,7 +176,7 @@ describe("/courseSubItems", () => {
 
     it("should return 400 if courseSubItems details are not valid", async () => {
       sub = {
-        ola:"pqrs"
+        ola: "pqrs",
       };
 
       const res = await exec();
@@ -136,11 +184,14 @@ describe("/courseSubItems", () => {
       expect(res.status).toBe(400);
     });
 
-    it('should return the courseSubItems if it is valid', async() => { 
+    it("should return the courseSubItems if it is valid", async () => {
       const res = await exec();
 
-      expect(res.body.data.courseSubItem).toHaveProperty('_id');
-      expect(res.body.data.courseSubItem).toHaveProperty("subItemTitle","Introduction%20to%20Programming");      
+      expect(res.body.data.courseSubItem).toHaveProperty("_id");
+      expect(res.body.data.courseSubItem).toHaveProperty(
+        "subItemTitle",
+        "Introduction%20to%20Programming"
+      );
     });
   });
 });
